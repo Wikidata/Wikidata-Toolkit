b68ca732bb0a9265b0701030ee06fbc3a911cd98
mkroetzsch <markus@semantic-mediawiki.org>
2014-01-25 16:16:28 +0100

    Update test to use JUnit 4 and set package properly

--- wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/implementation/ItemIdImplTest.java
+++ wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/implementation/ItemIdImplTest.java
@@ -1,3 +1,5 @@
+package org.wikidata.wdtk.datamodel.implementation;
+
 /*
  * #%L
  * Wikidata Toolkit Data Model
@@ -20,38 +22,46 @@
 import org.wikidata.wdtk.datamodel.implementation.ItemIdImpl;
 import org.wikidata.wdtk.datamodel.interfaces.EntityId;
 
-import junit.framework.TestCase;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-public class ItemIdImplTest extends TestCase {
+import org.junit.Test;
+import org.junit.Before;
+
+
+public class ItemIdImplTest {
 
 	private ItemIdImpl item1;
 	private ItemIdImpl item2;
 	private ItemIdImpl item3;
 	private ItemIdImpl item4;
 
-	protected void setUp() {
+	@Before
+	public void setUp() {
 		item1 = new ItemIdImpl("Q42", "http://www.wikidata.org/entity/");
 		item2 = new ItemIdImpl("Q42", "http://www.wikidata.org/entity/");
 		item3 = new ItemIdImpl("Q57", "http://www.wikidata.org/entity/");
 		item4 = new ItemIdImpl("Q42", "http://www.example.org/entity/");
 	}
 
-	public void testItemType() {
+	@Test
+	public void entityTypeIsItem() {
 		assertEquals(item1.getEntityType(), EntityId.EntityType.ITEM);
 	}
 
-	public void testItemIri() {
+	@Test
+	public void iriIsCorrect() {
 		assertEquals(item1.getIri(), "http://www.wikidata.org/entity/Q42");
 		assertEquals(item4.getIri(), "http://www.example.org/entity/Q42");
 	}
 
-	public void testItemId() {
+	@Test
+	public void idIsCorrect() {
 		assertEquals(item1.getId(), "Q42");
 	}
 
-	public void testItemEquality() {
+	@Test
+	public void itemEqualityBasedOnContent() {
 		assertEquals(item1, item2);
 		assertThat(item1, not(equalTo(item3)));
 		assertThat(item1, not(equalTo(item4)));
