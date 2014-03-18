214338879608ae20edfa6743a85de47ae089f8e4
Fredo Erxleben <fredo.erxleben@tu-dresden.de>
2014-03-18 13:23:47 +0100

    Added the universe test case.

--- wdtk-dumpfiles/src/test/java/org/wikidata/wdtk/dumpfiles/jsonConverterTest.java
+++ wdtk-dumpfiles/src/test/java/org/wikidata/wdtk/dumpfiles/jsonConverterTest.java
@@ -175,7 +175,9 @@ public class jsonConverterTest {
 
 		}
 		assertTrue(caughtException);
-
+		
+		ItemTestCase universe = this.generateItemTestCase("Universe.json");
+		universe.convert();
 	}
 
 	/**
