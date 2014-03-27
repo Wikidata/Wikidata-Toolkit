878bc6c30f614a2f4a53153907a6c4a0c4d4970c
Fredo Erxleben <fredo.erxleben@tu-dresden.de>
2014-03-27 16:16:05 +0100

    Copied TestObjectFactory to a more convenient place. Added reworked test outline.

--- wdtk-dumpfiles/src/test/java/org/wikidata/wdtk/dumpfiles/JsonConverterTest.java
+++ wdtk-dumpfiles/src/test/java/org/wikidata/wdtk/dumpfiles/JsonConverterTest.java
@@ -21,35 +21,18 @@ package org.wikidata.wdtk.dumpfiles;
  */
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.wikidata.wdtk.datamodel.implementation.DataObjectFactoryImpl;
-import org.wikidata.wdtk.datamodel.interfaces.Claim;
-import org.wikidata.wdtk.datamodel.interfaces.DataObjectFactory;
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
-import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
-import org.wikidata.wdtk.datamodel.interfaces.MonolingualTextValue;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
-import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
-import org.wikidata.wdtk.datamodel.interfaces.Reference;
-import org.wikidata.wdtk.datamodel.interfaces.SiteLink;
-import org.wikidata.wdtk.datamodel.interfaces.Snak;
-import org.wikidata.wdtk.datamodel.interfaces.Statement;
-import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
-import org.wikidata.wdtk.datamodel.interfaces.StatementRank;
-import org.wikidata.wdtk.datamodel.interfaces.Value;
-import org.wikidata.wdtk.datamodel.jsonconverter.TestObjectFactory;
 import org.wikidata.wdtk.dumpfiles.TestHelpers.JsonFetcher;
+import org.wikidata.wdtk.dumpfiles.TestHelpers.TestObjectFactory;
 
 /**
  * The test setup uses several files containing JSON. These files are read by
@@ -64,219 +47,103 @@ public class JsonConverterTest {
 	private static final String BASE_IRI = "test";
 	private static final TestObjectFactory testObjectFactory = new TestObjectFactory();
 
-	private JsonConverter unitUnderTest;
-	private DataObjectFactory factory;
+	private JsonConverter uut; // unit under test
 
 	@Before
 	public void setUp() {
-		this.factory = new DataObjectFactoryImpl();
-		this.unitUnderTest = new JsonConverter(BASE_IRI,
+		this.uut = new JsonConverter(BASE_IRI,
 				new DataObjectFactoryImpl());
 	}
 
 	@Test
 	public void testEmptyProperty() throws JSONException, IOException {
-		PropertyDocument propertyDocument = getPropertyDocumentFromResource(
-				"EmptyProperty.json", "P1");
+		PropertyDocument propertyDocument = jsonFetcher
+				.getPropertyDocumentFromResource("EmptyProperty.json", "P1",
+						this.uut);
 
-		PropertyDocument emptyPropertyDocument 
-		= testObjectFactory.createEmptyPropertyDocument(BASE_IRI);
+		PropertyDocument emptyPropertyDocument = testObjectFactory
+				.createEmptyPropertyDocument(BASE_IRI);
 
 		assertEquals(propertyDocument, emptyPropertyDocument);
 	}
 
-	// TODO rework
 	@Test
 	public void testEmptyItem() throws JSONException, IOException {
-		ItemDocument itemDocument = getItemDocumentFromResource(
-				"EmptyItem.json", "Q1");
+		ItemDocument itemDocument = jsonFetcher.getItemDocumentFromResource(
+				"EmptyItem.json", "Q1", this.uut);
 
-		ItemIdValue itemIdValue = this.factory.getItemIdValue("Q1", BASE_IRI);
-		Map<String, SiteLink> siteLinks = new HashMap<>();
-		ItemDocument emptyItemDocument = this.factory.getItemDocument(
-				itemIdValue, Collections.<MonolingualTextValue> emptyList(),
-				Collections.<MonolingualTextValue> emptyList(),
-				Collections.<MonolingualTextValue> emptyList(),
-				Collections.<StatementGroup> emptyList(), siteLinks);
+		ItemDocument emptyItemDocument = testObjectFactory
+				.createEmptyItemDocument(BASE_IRI);
 
 		assertEquals(itemDocument, emptyItemDocument);
 	}
 
-	// TODO rework or scrap
 	@Test
-	public void testBasicItem() throws JSONException, IOException {
-		ItemDocument basicItemDocument = this.createBasicItemDocument();
-		ItemDocument itemDocument = getItemDocumentFromResource(
-				"BasicItem.json", "Q1");
-
-		assertEquals(itemDocument.getEntityId(),
-				basicItemDocument.getEntityId());
-		assertEquals(itemDocument.getItemId(), basicItemDocument.getItemId());
-		assertEquals(itemDocument.getDescriptions(),
-				basicItemDocument.getDescriptions());
-		assertEquals(itemDocument.getAliases(), basicItemDocument.getAliases());
-		assertEquals(itemDocument.getLabels(), basicItemDocument.getLabels());
-		assertEquals(itemDocument.getSiteLinks(),
-				basicItemDocument.getSiteLinks());
-		assertEquals(itemDocument.getStatementGroups(),
-				basicItemDocument.getStatementGroups());
-
-		assertEquals(itemDocument, basicItemDocument);
+	public void testLabels(){
+		// TODO complete
+		fail("Empty test");
 	}
-
-	// TODO scrap?
-	private ItemDocument createBasicItemDocument() {
-
-		ItemIdValue itemIdValue = this.factory.getItemIdValue("Q1", BASE_IRI);
-
-		List<MonolingualTextValue> labels = new LinkedList<>();
-		labels.add(this.factory.getMonolingualTextValue("test", "en"));
-
-		List<MonolingualTextValue> descriptions = new LinkedList<>();
-		descriptions.add(this.factory.getMonolingualTextValue("this is a test",
-				"en"));
-
-		List<MonolingualTextValue> aliases = new LinkedList<>();
-		aliases.add(this.factory.getMonolingualTextValue("TEST", "en"));
-		aliases.add(this.factory.getMonolingualTextValue("Test", "en"));
-
-		List<StatementGroup> statementGroups = new LinkedList<>();
-		List<Statement> statements = new LinkedList<>();
-
-		PropertyIdValue propertyId = this.factory.getPropertyIdValue("P1",
-				BASE_IRI);
-		Value value = this.factory.getItemIdValue("Q1", BASE_IRI);
-		Snak mainSnak = factory.getValueSnak(propertyId, value);
-		List<? extends Snak> qualifiers = new LinkedList<>();
-		Claim claim = this.factory.getClaim(itemIdValue, mainSnak, qualifiers);
-
-		List<? extends Reference> references = new LinkedList<>();
-		StatementRank rank = StatementRank.NORMAL;
-		String statementId = "foo";
-		statements.add(this.factory.getStatement(claim, references, rank,
-				statementId));
-
-		statementGroups.add(this.factory.getStatementGroup(statements));
-
-		Map<String, SiteLink> siteLinks = new HashMap<>();
-		List<String> badges = new LinkedList<>();
-		String siteKey = "enwiki";
-		String title = "test";
-		siteLinks.put("enwiki",
-				this.factory.getSiteLink(title, siteKey, "", badges));
-
-		ItemDocument document = this.factory.getItemDocument(itemIdValue,
-				labels, descriptions, aliases, statementGroups, siteLinks);
-		return document;
-	}
-
-	// TODO rework or scrap
+	
 	@Test
-	public void testRealItems() throws JSONException, IOException {
-		getItemDocumentFromResource("Chicago.json", "Q1");
-		getItemDocumentFromResource("Haaften.json", "Q1");
-		getItemDocumentFromResource("Tours.json", "Q1");
-		getItemDocumentFromResource("JohnPaulII.json", "Q1");
-		getItemDocumentFromResource("Wernigerode.json", "Q1");
-		// FIXME this does not test anything (copied from earlier test file)
+	public void testDescriptions(){
+		// TODO complete
+		fail("Empty test");
 	}
-
+	
+	@Test
+	public void testAliases(){
+		// TODO complete
+		fail("Empty test");
+	}
+	
+	@Test
+	public void testLinks(){
+		// TODO complete
+		fail("Empty test");
+	}
+	
 	// TODO rework
 	@Test
 	public void testClaims() throws JSONException, IOException {
-		getItemDocumentFromResource("GlobalCoordinates.json", "Q1");
-		getItemDocumentFromResource("StatementRanks.json", "Q1");
-		getItemDocumentFromResource("SnakTypes.json", "Q1");
+		jsonFetcher.getItemDocumentFromResource("GlobalCoordinates.json", "Q1",
+				this.uut);
+		jsonFetcher.getItemDocumentFromResource("StatementRanks.json", "Q1", this.uut);
+		jsonFetcher.getItemDocumentFromResource("SnakTypes.json", "Q1", this.uut);
 		// FIXME this does not test anything (copied from earlier test file)
 	}
-
-	// TODO test different notations in the appropriate tests
+	
 	@Test
-	public void testDifferentNotations() throws JSONException, IOException {
-		getItemDocumentFromResource("DifferentNotations.json", "Q1");
-		getItemDocumentFromResource("StringEntityItem.json", "Q1");
-		getPropertyDocumentFromResource("StringEntityProperty.json", "P1");
-		// FIXME this does not test anything (copied from earlier test file)
+	public void testDatatype(){
+		// TODO complete
+		fail("Empty test");
 	}
 
 	// TODO improve
 	@Test(expected = JSONException.class)
 	public void testPropertyDocumentLacksDatatype() throws JSONException,
 			IOException {
-		getPropertyDocumentFromResource("NoEntityDocument.json", "P1");
+		jsonFetcher.getPropertyDocumentFromResource("NoEntityDocument.json", "P1", this.uut);
 	}
 
 	// TODO improve
 	@Test(expected = JSONException.class)
 	public void testItemDocumentWithErrors() throws JSONException, IOException {
-		getItemDocumentFromResource("MiscErrors.json", "Q1");
-	}
-	
-	// TODO remove, merge corner case with the other tests
-	@Test
-	public void testUniverse() throws JSONException, IOException {
-		getItemDocumentFromResource("Universe.json", "Q1");
-		// FIXME this does not test anything (copied from earlier test file)
+		jsonFetcher.getItemDocumentFromResource("MiscErrors.json", "Q1", this.uut);
 	}
 
-	// TODO move?
-	/**
-	 * Applies the JSON converter to the JSON stored in the given resource to
-	 * return an ItemDocument.
-	 * 
-	 * @param fileName
-	 *            the file name only, no path information
-	 * @param itemId
-	 *            the string id of the item
-	 * @throws IOException
-	 * @throws JSONException
-	 * @return the ItemDocument
-	 */
-	private ItemDocument getItemDocumentFromResource(String fileName,
-			String itemId) throws IOException, JSONException {
-		JSONObject jsonObject = jsonFetcher.getJsonObjectForResource(fileName);
-		return this.unitUnderTest.convertToItemDocument(jsonObject, itemId);
-	}
-
-	// TODO move?
-	/**
-	 * Applies the JSON converter to the JSON stored in the given resource to
-	 * return a PropertyDocument.
-	 * 
-	 * @param fileName
-	 *            the file name only, no path information. The file is supposed
-	 *            to be in the "resources/testSamples/"-directory.
-	 * @param propertyId
-	 *            the string id of the property
-	 * @return the PropertyDocument
-	 * @throws JSONException
-	 * @throws IOException
-	 */
-	private PropertyDocument getPropertyDocumentFromResource(String fileName,
-			String propertyId) throws IOException, JSONException {
-		JSONObject jsonObject = jsonFetcher.getJsonObjectForResource(fileName);
-		return this.unitUnderTest.convertToPropertyDocument(jsonObject,
-				propertyId);
-	}
-
-
-	//TODO:
-	//* empty item
-	//* empty property
-	//* Both
-	//	* label notations
-	//		* empty labels (via empty item)
-	//		* labels as strings
-	//		* labels as objects
-	//	* description notations
-	//	* aliases
-	//	* (entity)
-	//* Items only
-	//	* links
-	//	* claims
-	//* Properties only
-	//	* datatype
-
-	
+	// TODO:
+	// * Both
+	// 		* label notations
+	// 			* labels as strings
+	// 			* labels as objects
+	// 		* description notations
+	// 		* aliases
+	// 		* (entity)
+	// * Items only
+	// 		* links
+	// 		* claims
+	// 			* different snak types
+	// * Properties only
+	// 		* datatype
 
 }
--- wdtk-dumpfiles/src/test/java/org/wikidata/wdtk/dumpfiles/TestHelpers/JsonFetcher.java
+++ wdtk-dumpfiles/src/test/java/org/wikidata/wdtk/dumpfiles/TestHelpers/JsonFetcher.java
@@ -5,19 +5,23 @@ import java.net.URL;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
+import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
+import org.wikidata.wdtk.dumpfiles.JsonConverter;
 import org.wikidata.wdtk.testing.MockStringContentFactory;
 
 /**
  * A simple helper class to get JSON objects from a ressource file.
+ * 
  * @author Fredo Erxleben
- *
+ * 
  */
 public class JsonFetcher {
-	// NOTE: in the case of switching to fasterxml.jackson this class might be 
+	// NOTE: in the case of switching to fasterxml.jackson this class might be
 	// outfitted with more methods to accomodate different ways of in- / output
-	
+
 	private static final String SAMPLE_FILES_BASE_PATH = "/testSamples/";
-	
+
 	/**
 	 * Returns a JSON object for the JSON stored in the given resource.
 	 * 
@@ -29,12 +33,53 @@ public class JsonFetcher {
 	 */
 	public JSONObject getJsonObjectForResource(String resourceName)
 			throws IOException, JSONException {
-		
+
 		URL resourceUrl = this.getClass().getResource(
 				JsonFetcher.SAMPLE_FILES_BASE_PATH + resourceName);
 		String jsonString = MockStringContentFactory
 				.getStringFromUrl(resourceUrl);
 		return new JSONObject(jsonString);
+	}
+
+	/**
+	 * Applies the JSON converter to the JSON stored in the given resource to
+	 * return an ItemDocument.
+	 * 
+	 * @param fileName
+	 *            the file name only, no path information
+	 * @param itemId
+	 *            the string id of the item
+	 * @throws IOException
+	 * @throws JSONException
+	 * @return the ItemDocument
+	 */
+	public ItemDocument getItemDocumentFromResource(String fileName,
+			String itemId, JsonConverter unitUnderTest) throws IOException,
+			JSONException {
+		
+		JSONObject jsonObject = this.getJsonObjectForResource(fileName);
+		return unitUnderTest.convertToItemDocument(jsonObject, itemId);
+	}
+
+	/**
+	 * Applies the JSON converter to the JSON stored in the given resource to
+	 * return a PropertyDocument.
+	 * 
+	 * @param fileName
+	 *            the file name only, no path information. The file is supposed
+	 *            to be in the "resources/testSamples/"-directory.
+	 * @param propertyId
+	 *            the string id of the property
+	 * @return the PropertyDocument
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	public PropertyDocument getPropertyDocumentFromResource(String fileName,
+			String propertyId, JsonConverter unitUnderTest) throws IOException,
+			JSONException {
+		
+		JSONObject jsonObject = this.getJsonObjectForResource(fileName);
+		return unitUnderTest.convertToPropertyDocument(jsonObject, propertyId);
 	}
 
 }
