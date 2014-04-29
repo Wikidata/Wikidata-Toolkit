302283e4633e73f822d1afcddd27b3ca0cdbdb9d
Julian Mendez <mendez@tcs.inf.tu-dresden.de>
2014-04-29 18:37:02 +0200

    Update memory measurer

--- wdtk-examples/src/main/java/org/wikidata/wdtk/benchmarks/memory/DumpProcessingMemoryMeasurer.java
+++ wdtk-examples/src/main/java/org/wikidata/wdtk/benchmarks/memory/DumpProcessingMemoryMeasurer.java
@@ -23,32 +23,19 @@ package org.wikidata.wdtk.benchmarks.memory;
 import java.io.IOException;
 import java.util.Date;
 
-import org.apache.log4j.ConsoleAppender;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-import org.apache.log4j.PatternLayout;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessor;
 import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
 import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
 import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
-import org.wikidata.wdtk.dumpfiles.MwDumpFileProcessor;
+import org.wikidata.wdtk.dumpfiles.DumpProcessingController;
 import org.wikidata.wdtk.dumpfiles.MwRevision;
-import org.wikidata.wdtk.dumpfiles.MwRevisionDumpFileProcessor;
 import org.wikidata.wdtk.dumpfiles.MwRevisionProcessor;
-import org.wikidata.wdtk.dumpfiles.MwRevisionProcessorBroker;
 import org.wikidata.wdtk.dumpfiles.StatisticsMwRevisionProcessor;
-import org.wikidata.wdtk.dumpfiles.WikibaseRevisionProcessor;
-import org.wikidata.wdtk.dumpfiles.WmfDumpFileManager;
-import org.wikidata.wdtk.util.DirectoryManager;
-import org.wikidata.wdtk.util.DirectoryManagerImpl;
-import org.wikidata.wdtk.util.WebResourceFetcher;
-import org.wikidata.wdtk.util.WebResourceFetcherImpl;
+import org.wikidata.wdtk.examples.ExampleHelpers;
 
 /**
  * This class demonstrates how to write an application that downloads and
- * processes dumpfiles from Wikidata.org. It shows a rather pedestrian setup of
- * the whole processing pipeline. Much of this code will be the same or very
- * similar for other processing tasks, with only the last component changed.
+ * processes dumpfiles from Wikidata.org, that also measures the used memory.
  * 
  * @author Markus Kroetzsch
  * @author Julian Mendez
@@ -62,95 +49,26 @@ public class DumpProcessingMemoryMeasurer {
 	/** The string "wikidatawiki" identifies Wikidata.org */
 	public static final String WIKIDATAWIKI = "wikidatawiki";
 
+	public static final String REV_PROC_STAT = "revision processing statistics";
+
 	/** The following can also be set to another directory */
 	public static final String USER_DIR = "user.dir";
 
 	public static void main(String[] args) throws IOException {
-		configureLogging();
+		ExampleHelpers.configureLogging();
 		System.out.println(getDocumentation());
-		WmfDumpFileManager dumpFileManager = createDumpFileManager();
-		MwDumpFileProcessor dumpFileProcessor = createDumpFileProcessor();
-		dumpFileManager.processAllRecentRevisionDumps(dumpFileProcessor, true);
-	}
-
-	/**
-	 * Creates an object that manages dumpfiles published by the Wikimedia
-	 * Foundation. This object will check for available complete and incremental
-	 * dump files, both online and in a local download directory. It provides
-	 * direct access to the (decompressed) string content of these files.
-	 * <p>
-	 * The details in this method define which download directory is to be used,
-	 * which Wikimedia project we are interested in (Wikidata), and that we want
-	 * to allow online access (instead of using local files only).
-	 * 
-	 * @return dump file manager
-	 * @throws IOException
-	 *             if the download directory is not accessible
-	 */
-	private static WmfDumpFileManager createDumpFileManager()
-			throws IOException {
-
-		String downloadDirectory = System.getProperty(USER_DIR);
-		DirectoryManager downloadDirectoryManager = new DirectoryManagerImpl(
-				downloadDirectory);
-
-		// The following can be set to null for offline operation:
-		WebResourceFetcher webResourceFetcher = new WebResourceFetcherImpl();
-
-		return new WmfDumpFileManager(WIKIDATAWIKI, downloadDirectoryManager,
-				webResourceFetcher);
-	}
-
-	/**
-	 * Create an object that handles the complete processing of MediaWiki
-	 * dumpfiles. This processing consists of the following main steps:
-	 * 
-	 * <pre>
-	 * XML dump file -> page revisions -> item documents
-	 * </pre>
-	 * 
-	 * The objects handling each step are of type {@link MwDumpFileProcessor},
-	 * {@link MwRevisionProcessor}, and {@link EntityDocumentProcessor}. In each
-	 * case, the object on the left calls the object on the right whenever new
-	 * data is available. Therefore, the object on the right must be known to
-	 * the object on the left, so we set up the objects in reverse order.
-	 * <p>
-	 * Normally, there is exactly one processor of each type. In the code below,
-	 * we want to use two different objects to process revisions (one to analyse
-	 * Wikidata item information and one to gather basic statistics about all
-	 * revisions). To do this, we use a broker class that processes revisions to
-	 * distribute them further to any number of revision processors.
-	 * 
-	 * @return dump file processor
-	 */
-	private static MwDumpFileProcessor createDumpFileProcessor() {
-		EntityDocumentProcessor edpEntityStats = new EntityStatisticsProcessor();
-		MwRevisionProcessor rpEntityStats = new WikibaseRevisionProcessor(
-				edpEntityStats);
-		MwRevisionProcessor rpRevisionStats = new StatisticsMwRevisionProcessor(
-				"revision processing statistics", (int) REPORT_FREQUENCY);
-		MwRevisionProcessorBroker rpBroker = new MwRevisionProcessorBroker();
-		rpBroker.registerMwRevisionProcessor(rpEntityStats,
+		DumpProcessingController dumpProcessingController = new DumpProcessingController(
+				WIKIDATAWIKI);
+		EntityDocumentProcessor edpItemStats = new EntityStatisticsProcessor();
+		dumpProcessingController.registerEntityDocumentProcessor(edpItemStats,
 				MwRevision.MODEL_WIKIBASE_ITEM, true);
-		rpBroker.registerMwRevisionProcessor(rpEntityStats,
+		dumpProcessingController.registerEntityDocumentProcessor(edpItemStats,
 				MwRevision.MODEL_WIKIBASE_PROPERTY, true);
-		rpBroker.registerMwRevisionProcessor(rpRevisionStats, null, true);
-		return new MwRevisionDumpFileProcessor(rpBroker);
-	}
-
-	/**
-	 * Defines how messages should be logged. This method can be modified to
-	 * restrict the logging messages that are shown on the console or to change
-	 * their formatting. See the documentation of Log4J for details on how to do
-	 * this.
-	 */
-	private static void configureLogging() {
-		ConsoleAppender consoleAppender = new ConsoleAppender();
-		String pattern = "%d{yyyy-MM-dd HH:mm:ss} %-5p - %m%n";
-		consoleAppender.setLayout(new PatternLayout(pattern));
-		consoleAppender.setThreshold(Level.INFO);
-		consoleAppender.activateOptions();
-		Logger.getRootLogger().addAppender(consoleAppender);
+		MwRevisionProcessor rpRevisionStats = new StatisticsMwRevisionProcessor(
+				REV_PROC_STAT, (int) REPORT_FREQUENCY);
+		dumpProcessingController.registerMwRevisionProcessor(rpRevisionStats,
+				null, true);
+		dumpProcessingController.processAllRecentRevisionDumps();
 	}
 
 	/**
