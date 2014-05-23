703ec665a8d274cf21ca3b4c12905121caeef967
Michael Guenther <guenthermi50@yahoo.de>
2014-05-23 12:38:33 +0200

    change parser 	- reparsing instead of removing blank nodes

--- wdtk-rdf/src/test/java/org/wikidata/wdtk/rdf/RdfConverterTest.java
+++ wdtk-rdf/src/test/java/org/wikidata/wdtk/rdf/RdfConverterTest.java
@@ -22,14 +22,22 @@ package org.wikidata.wdtk.rdf;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openrdf.model.Model;
+import org.openrdf.model.impl.LinkedHashModel;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.RDFHandlerException;
+import org.openrdf.rio.RDFParseException;
+import org.openrdf.rio.RDFParser;
+import org.openrdf.rio.Rio;
+import org.openrdf.rio.helpers.StatementCollector;
 import org.wikidata.wdtk.datamodel.implementation.SitesImpl;
 import org.wikidata.wdtk.rdf.RdfConverter;
 import org.wikidata.wdtk.rdf.TestObjectFactory;
@@ -38,32 +46,27 @@ import org.wikidata.wdtk.testing.MockStringContentFactory;
 public class RdfConverterTest {
 
 	ByteArrayOutputStream out;
-	
+
 	RdfWriter rdfWriter;
 	RdfConverter rdfConverter;
-	
+
 	final TestObjectFactory objectFactory = new TestObjectFactory();
-	
-	public String getResourceFromFile(String fileName)
-			throws IOException {
-		return MockStringContentFactory.getStringFromUrl(this
-				.getClass().getResource("/" + fileName));
+
+	public String getResourceFromFile(String fileName) throws IOException {
+		return MockStringContentFactory.getStringFromUrl(this.getClass()
+				.getResource("/" + fileName));
 	}
-	
-	public String removeBlankNodes(String rdf){
-		StringBuilder builder = new StringBuilder();
-		int pos = 0;
-		do {
-			pos = rdf.indexOf("node");
-			if (pos != -1){
-				builder.append(rdf.substring(0, pos));
-				rdf = rdf.substring(pos + 15);
-			}
-		}while (pos != -1);
-		builder.append(rdf);
-		return builder.toString();
+
+	public Model parseRdf(String rdfResource) throws RDFParseException,
+			RDFHandlerException, IOException {
+		InputStream inStream = new ByteArrayInputStream(rdfResource.getBytes());
+		RDFParser parser = Rio.createParser(RDFFormat.N3);
+		parser.parse(inStream, "http://test/");
+		Model graph = new LinkedHashModel();
+		parser.setRDFHandler(new StatementCollector(graph));
+		return graph;
 	}
-	
+
 	@Before
 	public void setUp() throws Exception {
 		out = new ByteArrayOutputStream();
@@ -73,23 +76,30 @@ public class RdfConverterTest {
 	}
 
 	@Test
-	public void testWriteItemDocument() throws RDFHandlerException, IOException {
-		org.wikidata.wdtk.datamodel.interfaces.ItemDocument document = objectFactory.createItemDocument();
+	public void testWriteItemDocument() throws RDFHandlerException,
+			IOException, RDFParseException {
+		org.wikidata.wdtk.datamodel.interfaces.ItemDocument document = objectFactory
+				.createItemDocument();
 		rdfConverter.writeItemDocument(document);
 		rdfWriter.finish();
-		assertEquals(removeBlankNodes(out.toString()), getResourceFromFile("ItemDocument.rdf"));
+		Model model = parseRdf(out.toString());
+		assertEquals(model, parseRdf(getResourceFromFile("ItemDocument.rdf")));
 	}
-	
+
 	@Test
-	public void testWritePropertyDocument() throws RDFHandlerException, IOException{
-		org.wikidata.wdtk.datamodel.interfaces.PropertyDocument document = objectFactory.createEmptyPropertyDocument();
+	public void testWritePropertyDocument() throws RDFHandlerException,
+			RDFParseException, IOException {
+		org.wikidata.wdtk.datamodel.interfaces.PropertyDocument document = objectFactory
+				.createEmptyPropertyDocument();
 		rdfConverter.writePropertyDocument(document);
 		rdfWriter.finish();
-		assertEquals(removeBlankNodes(out.toString()), getResourceFromFile("EmptyPropertyDocument.rdf"));
+		Model model = parseRdf(out.toString());
+		assertEquals(model,
+				parseRdf(getResourceFromFile("EmptyPropertyDocument.rdf")));
 	}
-	
+
 	@After
-	public void clear() throws RDFHandlerException, IOException{
+	public void clear() throws RDFHandlerException, IOException {
 		out.close();
 	}
 
