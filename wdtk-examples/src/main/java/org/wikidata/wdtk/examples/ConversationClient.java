a16dcc237ad1786e768b186f035328b3697a6a7f
Michael Guenther <guenthermi50@yahoo.de>
2014-06-11 13:17:14 +0200

    implement standard json, rdf serialization 	not all arguments will be recognized yet

--- wdtk-examples/src/main/java/org/wikidata/wdtk/examples/ConversationClient.java
+++ wdtk-examples/src/main/java/org/wikidata/wdtk/examples/ConversationClient.java
@@ -1,5 +1,17 @@
 package org.wikidata.wdtk.examples;
 
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -8,8 +20,19 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.compress.compressors.bzip2.BZip2CompressorOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipParameters;
+import org.openrdf.rio.RDFFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.wikidata.wdtk.datamodel.interfaces.Sites;
+import org.wikidata.wdtk.datamodel.json.JsonSerializer;
+import org.wikidata.wdtk.dumpfiles.DumpProcessingController;
+import org.wikidata.wdtk.dumpfiles.MwRevision;
+import org.wikidata.wdtk.dumpfiles.MwRevisionProcessor;
+import org.wikidata.wdtk.dumpfiles.StatisticsMwRevisionProcessor;
+import org.wikidata.wdtk.rdf.RdfSerializer;
 
 /*
  * #%L
@@ -40,14 +63,271 @@ import org.slf4j.LoggerFactory;
  */
 public class ConversationClient {
 
+	final static String COMPRESS_BZ2 = ".bz2";
+	final static String COMPRESS_GZIP = ".gz";
+	final static String COMPRESS_NONE = "";
+
+	private static Sites sites;
+	private static DumpProcessingController dumpProcessingController;
+	private static List<RdfSerializer> serializers = new ArrayList<RdfSerializer>();
+	private static List<String> serializerNames = new ArrayList<String>();
+
 	Options options;
 
 	String outputFormat;
 	String outputDestination;
+	String dumplocation;
 	Boolean stdout = false;
+	Boolean offlineMode = false;
 
 	static final Logger logger = LoggerFactory
 			.getLogger(ConversationClient.class);
+
+	public void setupForRdfSerialization() throws IOException {
+		// Initialize sites; needed to link to Wikipedia pages in RDF
+		sites = dumpProcessingController.getSitesInformation();
+
+		String compressionExtension = COMPRESS_NONE;
+
+		if (!stdout) {
+			compressionExtension = COMPRESS_GZIP;
+		}
+
+		// Create serializers for several data parts and encodings:
+		createRdfSerializer("wikidata-properties.nt", compressionExtension,
+				RdfSerializer.TASK_PROPERTIES
+						| RdfSerializer.TASK_ALL_EXACT_DATA);
+		createRdfSerializer("wikidata-terms.nt", compressionExtension,
+				RdfSerializer.TASK_ITEMS | RdfSerializer.TASK_TERMS);
+		createRdfSerializer("wikidata-statements.nt", compressionExtension,
+				RdfSerializer.TASK_ITEMS | RdfSerializer.TASK_STATEMENTS);
+		createRdfSerializer("wikidata-simple-statements.nt",
+				compressionExtension, RdfSerializer.TASK_ITEMS
+						| RdfSerializer.TASK_SIMPLE_STATEMENTS);
+		createRdfSerializer("wikidata-taxonomy.nt", compressionExtension,
+				RdfSerializer.TASK_ITEMS | RdfSerializer.TASK_TAXONOMY);
+		createRdfSerializer("wikidata-instances.nt", compressionExtension,
+				RdfSerializer.TASK_ITEMS | RdfSerializer.TASK_INSTANCE_OF);
+		createRdfSerializer("wikidata-sitelinks.nt", compressionExtension,
+				RdfSerializer.TASK_ITEMS | RdfSerializer.TASK_SITELINKS);
+
+	}
+
+	public void setupForJsonSerialization() throws FileNotFoundException,
+			IOException {
+		// Write the output to a BZip2-compressed file
+		BZip2CompressorOutputStream outputStream = new BZip2CompressorOutputStream(
+				new FileOutputStream("WikidataDump.json.bz2"));
+		// Create an object for managing the serialization process
+		JsonSerializer serializer = new JsonSerializer(outputStream);
+
+		// Subscribe to the most recent entity documents of type wikibase item
+		// and property:
+		dumpProcessingController.registerEntityDocumentProcessor(serializer,
+				MwRevision.MODEL_WIKIBASE_ITEM, true);
+		dumpProcessingController.registerEntityDocumentProcessor(serializer,
+				MwRevision.MODEL_WIKIBASE_PROPERTY, true);
+	}
+
+	/**
+	 * Manages the serialization process.
+	 * 
+	 * @throws IOException
+	 */
+	public void convert() throws IOException {
+
+		if (!stdout) {
+			// Define where log messages go
+			ExampleHelpers.configureLogging();
+		}
+
+		// Controller object for processing dumps:
+		dumpProcessingController = new DumpProcessingController("wikidatawiki");
+
+		if (offlineMode) {
+			dumpProcessingController.setOfflineMode(true);
+		}
+
+		switch (outputFormat) {
+		case "json":
+			setupForJsonSerialization();
+		case "rdf":
+			setupForRdfSerialization();
+		default:
+			logger.warn("no output format or unknown output format specified - only statistics will be processed (except the case that stdout flag is set)");
+		}
+
+		if (!stdout) {
+			// General statistics and time keeping:
+			MwRevisionProcessor rpRevisionStats = new StatisticsMwRevisionProcessor(
+					"revision processing statistics", 10000);
+
+			// Subscribe to all current revisions (null = no filter):
+			dumpProcessingController.registerMwRevisionProcessor(
+					rpRevisionStats, null, true);
+		}
+
+		// Set up the serializer and write headers
+		startSerializers();
+
+		// Start processing (may trigger downloads where needed)
+		dumpProcessingController.processMostRecentMainDump();
+
+		// Finish the serialization
+		closeSerializers();
+	}
+
+	/**
+	 * Creates a new RDF Serializer. Output is written to the file of the given
+	 * name (it will always be a compressed file, so the name should reflect
+	 * that). The tasks define what the serializer will be writing into this
+	 * file. The new serializer is also registered in an internal list, so it
+	 * can be started and closed more conveniently.
+	 * 
+	 * @param outputFileName
+	 *            filename to write output to
+	 * @param compressionExtension
+	 *            the extension of the chosen compression format or the empty
+	 *            string for no compression
+	 * @param tasks
+	 *            an integer that is a bitwise OR of flags like
+	 *            {@link RdfSerializer#TASK_LABELS}.
+	 * @return the newly created serializer
+	 * @throws FileNotFoundException
+	 *             if the given file cannot be opened for writing for some
+	 *             reason
+	 * @throws IOException
+	 *             if it was not possible to write the BZ2 header to the file
+	 */
+	@SuppressWarnings("resource")
+	private RdfSerializer createRdfSerializer(String outputFileName,
+			String compressionExtension, int tasks)
+			throws FileNotFoundException, IOException {
+
+		OutputStream bufferedFileOutputStream = new BufferedOutputStream(
+				new FileOutputStream(outputFileName + compressionExtension),
+				1024 * 1024 * 5 * 0 + 100);
+
+		OutputStream compressorOutputStream = null;
+		switch (compressionExtension) {
+		case COMPRESS_BZ2:
+			compressorOutputStream = new BZip2CompressorOutputStream(
+					bufferedFileOutputStream);
+			break;
+		case COMPRESS_GZIP:
+			GzipParameters gzipParameters = new GzipParameters();
+			gzipParameters.setCompressionLevel(7);
+			compressorOutputStream = new GzipCompressorOutputStream(
+					bufferedFileOutputStream, gzipParameters);
+			break;
+		case COMPRESS_NONE:
+			compressorOutputStream = bufferedFileOutputStream;
+			break;
+		default:
+			throw new IllegalArgumentException(
+					"Unsupported compression format: " + compressionExtension);
+		}
+
+		OutputStream exportOutputStream;
+
+		if (stdout) {
+			exportOutputStream = System.out;
+		} else {
+			exportOutputStream = asynchronousOutputStream(compressorOutputStream);
+		}
+
+		RdfSerializer serializer = new RdfSerializer(RDFFormat.NTRIPLES,
+				exportOutputStream, sites);
+		serializer.setTasks(tasks);
+
+		dumpProcessingController.registerEntityDocumentProcessor(serializer,
+				MwRevision.MODEL_WIKIBASE_ITEM, true);
+		dumpProcessingController.registerEntityDocumentProcessor(serializer,
+				MwRevision.MODEL_WIKIBASE_PROPERTY, true);
+
+		serializers.add(serializer);
+		serializerNames.add(outputFileName);
+
+		return serializer;
+	}
+
+	/**
+	 * Starts the serializers. This includes the writing of headers if any (N3
+	 * has no headers, but other formats have).
+	 */
+	private static void startSerializers() {
+		for (RdfSerializer serializer : serializers) {
+			serializer.start();
+		}
+	}
+
+	/**
+	 * Closes the serializers (and their output streams), and prints a short
+	 * summary of the number of triples serialized by each.
+	 */
+	private static void closeSerializers() {
+		Iterator<String> nameIterator = serializerNames.iterator();
+		for (RdfSerializer serializer : serializers) {
+			serializer.close();
+			System.out.println("*** Finished serialization of "
+					+ serializer.getTripleCount() + " RDF triples in file "
+					+ nameIterator.next());
+		}
+	}
+
+	/**
+	 * Creates a separate thread for writing into the given output stream and
+	 * returns a pipe output stream that can be used to pass data to this
+	 * thread.
+	 * <p>
+	 * This code is inspired by
+	 * http://stackoverflow.com/questions/12532073/gzipoutputstream
+	 * -that-does-its-compression-in-a-separate-thread
+	 * 
+	 * @param outputStream
+	 *            the stream to write to in the thread
+	 * @return a new stream that data should be written to
+	 * @throws IOException
+	 *             if the pipes could not be created for some reason
+	 */
+	public static OutputStream asynchronousOutputStream(
+			final OutputStream outputStream) throws IOException {
+		final int SIZE = 1024 * 1024 * 10;
+		final PipedOutputStream pos = new PipedOutputStream();
+		final PipedInputStream pis = new PipedInputStream(pos, SIZE);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					byte[] bytes = new byte[SIZE];
+					for (int len; (len = pis.read(bytes)) > 0;) {
+						outputStream.write(bytes, 0, len);
+					}
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+				} finally {
+					close(pis);
+					close(outputStream);
+				}
+			}
+		}, "async-output-stream").start();
+		return pos;
+	}
+
+	/**
+	 * Closes a Closeable and swallows any exceptions that might occur in the
+	 * process.
+	 * 
+	 * @param closeable
+	 */
+	static void close(Closeable closeable) {
+		if (closeable != null) {
+			try {
+				closeable.close();
+			} catch (IOException ignored) {
+			}
+		}
+	}
 
 	public Options getOptions() {
 		return options;
@@ -72,9 +352,15 @@ public class ConversationClient {
 				.withDescription(
 						"place the output into the directory located at <path>")
 				.withLongOpt("destination").create("d");
+		Option dumplocation = OptionBuilder.withArgName("path").hasArg()
+				.withDescription("defines the location of the dumpfiles")
+				.withLongOpt("dumplocation").create("l");
 
 		options.addOption(format);
 		options.addOption(destination);
+		options.addOption(dumplocation);
+		options.addOption("n", "offline mode", false,
+				"offline mode - converter should use only previousely downloaded dumps");
 		options.addOption("h", "help", false, "print this message");
 
 		options.addOption("s", "stdout", false, "write output to stdout");
@@ -111,11 +397,21 @@ public class ConversationClient {
 		} else {
 			logger.warn("No output format specified!");
 		}
+
+		if (cmd.hasOption("l")) {
+			dumplocation = cmd.getOptionValue("l");
+		}
+
+		if (cmd.hasOption("n")) {
+			offlineMode = true;
+		}
+
 	}
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) throws ParseException, IOException {
 		ConversationClient client = new ConversationClient();
 		client.addOptions();
 		client.handleArguments(args);
+		client.convert();
 	}
 }
