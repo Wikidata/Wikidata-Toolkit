c5c3673dc71cd99876dcd00067fd45bda0095212
Markus Kroetzsch <markus@semantic-mediawiki.org>
2014-08-31 17:28:15 +0200

    Minor formatting changes

--- wdtk-storage/src/main/java/org/wikidata/wdtk/storage/wdtktodb/TermsPropertyTargetIterator.java
+++ wdtk-storage/src/main/java/org/wikidata/wdtk/storage/wdtktodb/TermsPropertyTargetIterator.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.storage.wdtktodb;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -83,7 +83,7 @@ public class TermsPropertyTargetIterator {
 			return new TermsAsPropertyTargets(WdtkSorts.PROP_ALIAS,
 					new NestedIterator<MonolingualTextValue>(
 							this.termedDocument.getAliases().values()),
-							targetCount, WdtkSorts.SORT_ALIAS);
+					targetCount, WdtkSorts.SORT_ALIAS);
 		} else {
 			return null;
 		}
