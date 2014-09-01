2cad09e5aa59d8674ec18d7302fc5dfb4ef15a12
Fredo Erxleben <fredo.erxleben@tu-dresden.de>
2014-09-01 14:08:47 +0200

    Speedtest finetuning

--- wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/Speedtest.java
+++ wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/Speedtest.java
@@ -1,5 +1,25 @@
 package org.wikidata.wdtk.datamodel;
 
+/*
+ * #%L
+ * Wikidata Toolkit Data Model
+ * %%
+ * Copyright (C) 2014 Wikidata Toolkit Developers
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -20,7 +40,6 @@ import org.wikidata.wdtk.datamodel.json.JsonSerializer;
 import org.wikidata.wdtk.datamodel.json.jackson.documents.ItemDocumentImpl;
 import org.wikidata.wdtk.util.Timer;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Speedtest {
