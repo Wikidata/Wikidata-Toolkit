1a47825f6d5afdb33ba992c462cbce85df2cee88
Julian Mendez <mendez@tcs.inf.tu-dresden.de>
2014-09-22 16:12:23 +0200

    Remove classes that do not render constraints

--- wdtk-dumpfiles/src/main/java/org/wikidata/wdtk/dumpfiles/constraint/renderer/ConstraintMainRenderer.java
+++ wdtk-dumpfiles/src/main/java/org/wikidata/wdtk/dumpfiles/constraint/renderer/ConstraintMainRenderer.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.dumpfiles.constraint.renderer;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -79,19 +79,16 @@ public class ConstraintMainRenderer implements ConstraintVisitor<Boolean> {
 
 	@Override
 	public Boolean visit(ConstraintSymmetric constraint) {
-		(new ConstraintSymmetricRenderer()).render(constraint);
 		return true;
 	}
 
 	@Override
 	public Boolean visit(ConstraintInverse constraint) {
-		(new ConstraintInverseRenderer()).render(constraint);
 		return true;
 	}
 
 	@Override
 	public Boolean visit(ConstraintExistingFile constraint) {
-		(new ConstraintExistingFileRenderer()).render(constraint);
 		return true;
 	}
 
@@ -130,7 +127,6 @@ public class ConstraintMainRenderer implements ConstraintVisitor<Boolean> {
 
 	@Override
 	public Boolean visit(ConstraintQualifier constraint) {
-		(new ConstraintQualifierRenderer()).render(constraint);
 		return true;
 	}
 
