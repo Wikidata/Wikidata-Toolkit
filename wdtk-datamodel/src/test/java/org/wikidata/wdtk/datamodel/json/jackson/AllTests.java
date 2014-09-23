0bd1fc35ce4742ae4c9b5c432c922a67dff78ddf
Markus Kroetzsch <markus@semantic-mediawiki.org>
2014-09-23 17:21:54 +0200

    Automatic code layout fixes

--- wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/json/jackson/AllTests.java
+++ wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/json/jackson/AllTests.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.datamodel.json.jackson;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,24 +20,18 @@ package org.wikidata.wdtk.datamodel.json.jackson;
  * #L%
  */
 
-import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  * A test suite that runs all the JSON conversion tests.
- * @author Fredo Erxleben
  *
+ * @author Fredo Erxleben
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ 
-	TestItemDocument.class, 
-	TestMonolingualTextValue.class,
-	TestSiteLink.class, 
-	TestSnakJson.class, 
-	TestValue.class, 
-	TestStatement.class,
-	TestDatatypeId.class,
-	TestAliasBug.class})
+@Suite.SuiteClasses({ TestItemDocument.class, TestMonolingualTextValue.class,
+		TestSiteLink.class, TestSnakJson.class, TestValue.class,
+		TestStatement.class, TestDatatypeId.class, TestAliasBug.class })
 public class AllTests {
 	// nothing
 }
--- wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/json/jackson/JsonComparator.java
+++ wdtk-datamodel/src/test/java/org/wikidata/wdtk/datamodel/json/jackson/JsonComparator.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.datamodel.json.jackson;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,9 +30,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * A helper class for comparing JSON objects to each other.
- * 
- * @author fredo
  *
+ * @author Fredo Erxleben
  */
 public class JsonComparator {
 
@@ -42,7 +41,7 @@ public class JsonComparator {
 	 * Compares two JSON objects represented by Strings to each other. Both
 	 * Strings are supposed to be valid JSON. From the given Strings the JSON
 	 * tree is build and both trees are compared.
-	 * 
+	 *
 	 * @param string1
 	 * @param string2
 	 */
