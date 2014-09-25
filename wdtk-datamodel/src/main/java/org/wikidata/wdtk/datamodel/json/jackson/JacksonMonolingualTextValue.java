f70a2ef2b867f82b8d8cc862b8a37caff3e85e07
Markus Kroetzsch <markus@semantic-mediawiki.org>
2014-09-25 09:33:44 +0200

    Reviewed and documented

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/json/jackson/JacksonMonolingualTextValue.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/json/jackson/JacksonMonolingualTextValue.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.datamodel.json.jackson;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,42 +25,84 @@ import org.wikidata.wdtk.datamodel.helpers.Hash;
 import org.wikidata.wdtk.datamodel.helpers.ToString;
 import org.wikidata.wdtk.datamodel.interfaces.MonolingualTextValue;
 import org.wikidata.wdtk.datamodel.interfaces.ValueVisitor;
+import org.wikidata.wdtk.datamodel.json.jackson.datavalues.JacksonValueMonolingualText;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- * A one-on-one representation of the external Json's monolingual text values.
- * Java attributes are named equally to the JSON fields. Deviations are due to
- * different naming in the implemented interfaces.
- * 
+ * Jackson representation of {@link MonolingualTextValue} data used in labels,
+ * aliases, and descriptions in JSON. Note that this is distinct from the JSON
+ * representation for property values of type {@link MonolingualTextValue},
+ * which is implemented in {@link JacksonValueMonolingualText}.
+ *
  * @author Fredo Erxleben
  *
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class JacksonMonolingualTextValue implements MonolingualTextValue {
 
+	/**
+	 * The language code.
+	 */
+	private String languageCode;
+	/**
+	 * The text value.
+	 */
+	private String text;
+
+	/**
+	 * Constructor. Creates an empty object that can be populated during JSON
+	 * deserialization. Should only be used by Jackson for this very purpose.
+	 */
 	public JacksonMonolingualTextValue() {
 	}
 
-	public JacksonMonolingualTextValue(String language, String value) {
-		this.language = language;
-		this.value = value;
+	/**
+	 * Create a new object from the given data.
+	 *
+	 * @param languageCode
+	 *            the language code of the value
+	 * @param text
+	 *            the text content of the value
+	 */
+	public JacksonMonolingualTextValue(String languageCode, String text) {
+		this.languageCode = languageCode;
+		this.text = text;
 	}
 
+	/**
+	 * Copy constructor.
+	 *
+	 * @param mltv
+	 *            the object to copy the data from
+	 */
 	public JacksonMonolingualTextValue(MonolingualTextValue mltv) {
 		this(mltv.getLanguageCode(), mltv.getText());
 	}
 
-	String language;
-	String value;
-
-	public void setLanguage(String language) {
-		this.language = language;
+	/**
+	 * Sets the language code to the given value. Only for use by Jackson during
+	 * deserialization.
+	 *
+	 * @param languageCode
+	 *            new value
+	 */
+	@JsonProperty("language")
+	public void setLanguageCode(String languageCode) {
+		this.languageCode = languageCode;
 	}
 
-	public void setValue(String value) {
-		this.value = value;
+	/**
+	 * Sets the text to the given value. Only for use by Jackson during
+	 * deserialization.
+	 *
+	 * @param text
+	 *            new value
+	 */
+	@JsonProperty("value")
+	public void setText(String text) {
+		this.text = text;
 	}
 
 	@Override
@@ -71,13 +113,13 @@ public class JacksonMonolingualTextValue implements MonolingualTextValue {
 	@JsonProperty("value")
 	@Override
 	public String getText() {
-		return this.value;
+		return this.text;
 	}
 
 	@JsonProperty("language")
 	@Override
 	public String getLanguageCode() {
-		return this.language;
+		return this.languageCode;
 	}
 
 	@Override
