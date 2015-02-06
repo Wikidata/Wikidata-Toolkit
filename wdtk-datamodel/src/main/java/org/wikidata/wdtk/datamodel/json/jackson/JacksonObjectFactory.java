68c72d1d1af10ea0a52145cce143dabaf357f6c7
Markus Kroetzsch <markus@semantic-mediawiki.org>
2015-02-06 14:00:53 +0100

    Added missing datatype definitions

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/json/jackson/JacksonObjectFactory.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/json/jackson/JacksonObjectFactory.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.datamodel.json.jackson;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -352,6 +352,12 @@ public class JacksonObjectFactory implements DataObjectFactory {
 			break;
 		case DatatypeIdValue.DT_STRING:
 			result.setJsonDatatype(JacksonDatatypeId.JSON_DT_STRING);
+			break;
+		case DatatypeIdValue.DT_MONOLINGUAL_TEXT:
+			result.setJsonDatatype(JacksonDatatypeId.JSON_DT_MONOLINGUAL_TEXT);
+			break;
+		case DatatypeIdValue.DT_PROPERTY:
+			result.setJsonDatatype(JacksonDatatypeId.JSON_DT_PROPERTY);
 			break;
 		default:
 			throw new IllegalArgumentException("Unknown datatype: "
