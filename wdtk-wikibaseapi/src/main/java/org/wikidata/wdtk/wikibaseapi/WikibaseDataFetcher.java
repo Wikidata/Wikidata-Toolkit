d0bd4ecd664ec4eb63c4abdb703b618a44fe9846
Markus Kroetzsch <markus@semantic-mediawiki.org>
2015-03-01 22:54:13 +0100

    Document limitation in property filtering

--- wdtk-wikibaseapi/src/main/java/org/wikidata/wdtk/wikibaseapi/WikibaseDataFetcher.java
+++ wdtk-wikibaseapi/src/main/java/org/wikidata/wdtk/wikibaseapi/WikibaseDataFetcher.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.wikibaseapi;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -108,6 +108,10 @@ public class WikibaseDataFetcher {
 	 * Returns the {@link DocumentDataFilter} object that is used to filter API
 	 * requests. Settings made in this object will affect the API request, and
 	 * often lead to reduced network traffic and better performance.
+	 * <p>
+	 * Note: Filtering individual properties is currently not supported (such
+	 * filters will be ignored). However, filtering all properties is possible;
+	 * in this case all statements are excluded.
 	 *
 	 * @return the filter used by this object
 	 */
