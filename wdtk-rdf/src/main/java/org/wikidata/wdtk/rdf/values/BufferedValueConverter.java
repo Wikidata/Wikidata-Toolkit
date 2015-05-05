9805f08c733261c5cfeda665d1f63ee7827849b2
Markus Kroetzsch <markus@semantic-mediawiki.org>
2015-05-05 17:52:16 +0200

    Fix for issue #85

--- wdtk-rdf/src/main/java/org/wikidata/wdtk/rdf/values/BufferedValueConverter.java
+++ wdtk-rdf/src/main/java/org/wikidata/wdtk/rdf/values/BufferedValueConverter.java
@@ -27,8 +27,8 @@ import java.util.List;
 
 import org.openrdf.model.Resource;
 import org.openrdf.rio.RDFHandlerException;
-import org.wikidata.wdtk.rdf.PropertyTypes;
 import org.wikidata.wdtk.rdf.OwlDeclarationBuffer;
+import org.wikidata.wdtk.rdf.PropertyTypes;
 import org.wikidata.wdtk.rdf.RdfWriter;
 
 public abstract class BufferedValueConverter<V extends org.wikidata.wdtk.datamodel.interfaces.Value>
@@ -39,7 +39,8 @@ public abstract class BufferedValueConverter<V extends org.wikidata.wdtk.datamod
 	final HashSet<Resource> declaredValues;
 
 	public BufferedValueConverter(RdfWriter rdfWriter,
-			PropertyTypes propertyTypes, OwlDeclarationBuffer rdfConversionBuffer) {
+			PropertyTypes propertyTypes,
+			OwlDeclarationBuffer rdfConversionBuffer) {
 		super(rdfWriter, propertyTypes, rdfConversionBuffer);
 		this.valueQueue = new ArrayList<V>();
 		this.valueSubjectQueue = new ArrayList<Resource>();
@@ -49,7 +50,7 @@ public abstract class BufferedValueConverter<V extends org.wikidata.wdtk.datamod
 	/**
 	 * Adds the given value to the list of values that should still be
 	 * serialized. The given RDF resource will be used as a subject.
-	 * 
+	 *
 	 * @param value
 	 *            the value to be serialized
 	 * @param resource
@@ -65,6 +66,7 @@ public abstract class BufferedValueConverter<V extends org.wikidata.wdtk.datamod
 		Iterator<V> valueIterator = this.valueQueue.iterator();
 		for (Resource resource : this.valueSubjectQueue) {
 			if (!this.declaredValues.add(resource)) {
+				valueIterator.next();
 				continue;
 			}
 			writeValue(valueIterator.next(), resource);
@@ -76,7 +78,7 @@ public abstract class BufferedValueConverter<V extends org.wikidata.wdtk.datamod
 	/**
 	 * Writes the triples for a single value, using the given resource as
 	 * subject.
-	 * 
+	 *
 	 * @param value
 	 * @param resource
 	 * @throws RDFHandlerException
