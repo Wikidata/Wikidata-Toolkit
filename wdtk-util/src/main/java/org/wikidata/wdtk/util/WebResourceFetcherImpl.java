1ea5c431b6acef150d6f4638e0f46dc23b8b7340
Markus Damm <nullachtfuffzn@arcor.de>
2015-06-14 19:06:01 +0200

    added proxy to WebResourceFetcherImpl

--- wdtk-util/src/main/java/org/wikidata/wdtk/util/WebResourceFetcherImpl.java
+++ wdtk-util/src/main/java/org/wikidata/wdtk/util/WebResourceFetcherImpl.java
@@ -22,6 +22,7 @@ package org.wikidata.wdtk.util;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.Proxy;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -36,9 +37,19 @@ public class WebResourceFetcherImpl implements WebResourceFetcher {
 	protected static String userAgent = "Wikidata Toolkit; Java "
 			+ System.getProperty("java.version");
 
+	protected static Proxy proxy = null;
+
+	public static Proxy getProxy() {
+		return proxy;
+	}
+
+	public static void setProxy(Proxy proxy) {
+		WebResourceFetcherImpl.proxy = proxy;
+	}
+
 	/**
-	 * Returns the string that will be used to ideintify the user agent on all
-	 * requests made by Wikidata Toolkit.
+	 * Returns the string that will be used to identify the user agent on
+	 * all requests made by Wikidata Toolkit.
 	 *
 	 * @return the user agent string
 	 */
@@ -47,12 +58,12 @@ public class WebResourceFetcherImpl implements WebResourceFetcher {
 	}
 
 	/**
-	 * Sets the string that will be used to ideintify the user agent on all
-	 * requests made by Wikidata Toolkit. This should be set in own tools based
-	 * on Wikidata Toolkit esp. when making large amounts of requests.
+	 * Sets the string that will be used to identify the user agent on all
+	 * requests made by Wikidata Toolkit. This should be set in own tools
+	 * based on Wikidata Toolkit esp. when making large amounts of requests.
 	 *
 	 * @param userAgent
-	 *            the user agent string
+	 *                the user agent string
 	 */
 	public static void setUserAgent(String userAgent) {
 		WebResourceFetcherImpl.userAgent = userAgent;
@@ -62,7 +73,12 @@ public class WebResourceFetcherImpl implements WebResourceFetcher {
 	public InputStream getInputStreamForUrl(String urlString)
 			throws IOException {
 		URL url = new URL(urlString);
-		URLConnection urlConnection = url.openConnection();
+		URLConnection urlConnection;
+		if (proxy == null) {
+			urlConnection = url.openConnection();
+		} else {
+			urlConnection = url.openConnection(proxy);
+		}
 		urlConnection.setRequestProperty("User-Agent", userAgent);
 
 		return urlConnection.getInputStream();
