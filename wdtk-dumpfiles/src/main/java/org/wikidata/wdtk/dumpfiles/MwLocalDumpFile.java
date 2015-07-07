47b30b5259d082c10b83409794798fdacacc024d
Markus Damm <nullachtfuffzn@arcor.de>
2015-07-07 14:27:46 +0200

    implementation of MwLocalDumpFile

--- wdtk-dumpfiles/src/main/java/org/wikidata/wdtk/dumpfiles/MwLocalDumpFile.java
+++ wdtk-dumpfiles/src/main/java/org/wikidata/wdtk/dumpfiles/MwLocalDumpFile.java
@@ -3,65 +3,107 @@ package org.wikidata.wdtk.dumpfiles;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.wikidata.wdtk.util.CompressionType;
 import org.wikidata.wdtk.util.DirectoryManager;
 
+/**
+ * Class for representing dump files that has been previously downladed
+ * 
+ * @author Markus Damm
+ *
+ */
 public class MwLocalDumpFile implements MwDumpFile {
 
 	protected final String dateStamp;
 	protected final String projectName;
+	Boolean isDone;
+	protected final String dumpFileName;
 
+	/**
+	 * DirectoryManager for this local dumpfile
+	 */
 	final DirectoryManager localDumpfileDirectoryManager;
 
+	/**
+	 * Type of this dumpfile
+	 */
 	final DumpContentType dumpContentType;
 
-	public MwLocalDumpFile(String dateStamp, String projectName,
-			DirectoryManager dumpFileDirectoryManager,
-			DumpContentType dumpContentType) {
+	/**
+	 * Hash map defining the compression type of each type of dump.
+	 */
+	static final Map<DumpContentType, CompressionType> COMPRESSION_TYPE = new HashMap<DumpContentType, CompressionType>();
+	static {
+		MwLocalDumpFile.COMPRESSION_TYPE.put(DumpContentType.DAILY,
+				CompressionType.BZ2);
+		MwLocalDumpFile.COMPRESSION_TYPE.put(DumpContentType.CURRENT,
+				CompressionType.BZ2);
+		MwLocalDumpFile.COMPRESSION_TYPE.put(DumpContentType.FULL,
+				CompressionType.BZ2);
+		MwLocalDumpFile.COMPRESSION_TYPE.put(DumpContentType.SITES,
+				CompressionType.GZIP);
+		MwLocalDumpFile.COMPRESSION_TYPE.put(DumpContentType.JSON,
+				CompressionType.GZIP);
+	}
+
+	public MwLocalDumpFile(DirectoryManager dumpFileDirectoryManager,
+			DumpContentType dumpContentType, String dumpFileName,
+			String dateStamp,
+			String projectName) {
 		this.dateStamp = dateStamp;
 		this.projectName = projectName;
 		this.localDumpfileDirectoryManager = dumpFileDirectoryManager;
 		this.dumpContentType = dumpContentType;
-
+		this.dumpFileName = dumpFileName;
 	}
 
 	@Override
 	public boolean isAvailable() {
-		// TODO Auto-generated method stub
-		return false;
+		if (this.isDone == null) {
+			isDone = this.localDumpfileDirectoryManager
+					.hasFile(dumpFileName);
+		}
+		return isDone;
 	}
 
 	@Override
 	public String getProjectName() {
-		return projectName;
+		return this.projectName;
 	}
 
 	@Override
 	public String getDateStamp() {
-		return dateStamp;
+		return this.dateStamp;
 	}
 
 	@Override
 	public DumpContentType getDumpContentType() {
-		return dumpContentType;
+		return this.dumpContentType;
 	}
 
 	@Override
 	public InputStream getDumpFileStream() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return this.localDumpfileDirectoryManager
+				.getInputStreamForFile(
+						dumpFileName,
+						MwLocalDumpFile.COMPRESSION_TYPE
+								.get(dumpContentType));
 	}
 
 	@Override
 	public BufferedReader getDumpFileReader() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		return new BufferedReader(new InputStreamReader(
+				getDumpFileStream(), StandardCharsets.UTF_8));
 	}
 
 	@Override
 	public void prepareDumpFile() throws IOException {
-		// TODO Auto-generated method stub
-
+		// nothing to do
 	}
 
 }
