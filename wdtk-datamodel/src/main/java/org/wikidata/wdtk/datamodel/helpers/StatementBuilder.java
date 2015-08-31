7c1bdcec17261572dd595813df56c354c48924b7
Markus Kroetzsch <markus@semantic-mediawiki.org>
2015-08-31 11:21:10 +0200

    Use more general type

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/helpers/StatementBuilder.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/helpers/StatementBuilder.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.datamodel.helpers;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -250,7 +250,7 @@ public class StatementBuilder extends
 	 *            the references to be added
 	 * @return builder object to continue construction
 	 */
-	public StatementBuilder withReferences(List<Reference> references) {
+	public StatementBuilder withReferences(List<? extends Reference> references) {
 		this.references.addAll(references);
 		return getThis();
 	}
