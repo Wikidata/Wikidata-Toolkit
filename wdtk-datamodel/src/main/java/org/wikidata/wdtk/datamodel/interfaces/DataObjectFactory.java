3e6bc1600733525264bbe5149dfbd7773d43a082
Markus Kroetzsch <markus@semantic-mediawiki.org>
2015-09-05 12:22:32 +0200

    Code layout; removed another superfluous "public"

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/interfaces/DataObjectFactory.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/interfaces/DataObjectFactory.java
@@ -161,7 +161,7 @@ public interface DataObjectFactory {
 	 *            the upper bound of the numeric value of this quantity
 	 * @return a {@link QuantityValue} corresponding to the input
 	 */
-	public QuantityValue getQuantityValue(BigDecimal numericValue,
+	QuantityValue getQuantityValue(BigDecimal numericValue,
 			BigDecimal lowerBound, BigDecimal upperBound);
 
 	/**
@@ -179,7 +179,7 @@ public interface DataObjectFactory {
 	 * @return a {@link QuantityValue} corresponding to the input
 	 */
 	QuantityValue getQuantityValue(BigDecimal numericValue,
-								   BigDecimal lowerBound, BigDecimal upperBound, String unit);
+			BigDecimal lowerBound, BigDecimal upperBound, String unit);
 
 	/**
 	 * Creates a {@link ValueSnak}.
