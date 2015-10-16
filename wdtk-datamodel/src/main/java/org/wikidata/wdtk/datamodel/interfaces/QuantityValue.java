3487ef05d3a4e09ef777a55917b5991dc54c55d4
Markus Kroetzsch <markus@semantic-mediawiki.org>
2015-10-16 16:02:34 +0200

    Update documentation: units are already implemented now

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/interfaces/QuantityValue.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/interfaces/QuantityValue.java
@@ -9,9 +9,9 @@ package org.wikidata.wdtk.datamodel.interfaces;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,10 +26,6 @@ import java.math.BigDecimal;
  * A quantity value represents a number, possibly under some unit. The number
  * can be of arbitrary precision. Additional upper and lower bounds are provided
  * to allow an interval of uncertainty to be defined.
- * <p>
- * As of Jan 2014, units of measurement are not supported yet. The interface may
- * be extended to account for this in the future. It is intended to use external
- * definitions for units and to identify units by wikidata.org entity ids.
  *
  * @author Markus Kroetzsch
  *
