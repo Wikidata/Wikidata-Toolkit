c89628b2386b0c106c2372636e61f96293ccd831
Tpt <thomaspt@hotmail.fr>
2018-03-17 21:55:29 +0100

    Moves AliasesDeserializer as an inner class

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/implementation/TermedStatementDocumentImpl.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/implementation/TermedStatementDocumentImpl.java
@@ -28,8 +28,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.commons.lang3.Validate;
-import org.wikidata.wdtk.datamodel.implementation.json.AliasesDeserializer;
 import org.wikidata.wdtk.datamodel.implementation.json.JacksonPreStatement;
 import org.wikidata.wdtk.datamodel.interfaces.EntityDocument;
 import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
@@ -359,4 +363,48 @@ public abstract class TermedStatementDocumentImpl implements
 		return map;
 	}
 
+	/**
+	 * A deserializer implementation for the aliases in an
+	 * {@link TermedStatementDocumentImpl}.
+	 * <p>
+	 * It implements a workaround to cope with empty aliases being represented as
+	 * <code>"aliases":[]</code> despite its declaration as map and not as list or
+	 * array. This is neither nice nor fast, and should be obsolete as soon as
+	 * possible.
+	 *
+	 */
+	static class AliasesDeserializer extends JsonDeserializer<Map<String, List<MonolingualTextValue>>> {
+
+		@Override
+		public Map<String, List<MonolingualTextValue>> deserialize(
+				JsonParser jp, DeserializationContext ctxt) throws JsonMappingException {
+
+			Map<String, List<MonolingualTextValue>> contents = new HashMap<>();
+
+			try {
+				JsonNode node = jp.getCodec().readTree(jp);
+				if (!node.isArray()) {
+					Iterator<Entry<String, JsonNode>> nodeIterator = node.fields();
+					while (nodeIterator.hasNext()) {
+						List<MonolingualTextValue> mltvList = new ArrayList<>();
+						Entry<String, JsonNode> currentNode = nodeIterator.next();
+						// get the list of MLTVs
+						for (JsonNode mltvEntry : currentNode.getValue()) {
+							String language = mltvEntry.get("language").asText();
+							String value = mltvEntry.get("value").asText();
+							mltvList.add(new TermImpl(language,value));
+						}
+
+						contents.put(currentNode.getKey(), mltvList);
+					}
+				}
+			} catch (Exception e) {
+				throw new JsonMappingException(jp, "Unexpected alias list serialization", e);
+			}
+
+			return contents;
+
+		}
+
+	}
 }
