cbab2e7fc97cef4b7c3647113cbf850b6e4e0203
Tpt <thomas@pellissier-tanon.fr>
2018-04-12 02:50:34 +0200

    Drops DatamodelConverter usage from JsonSerializationProcessor

--- wdtk-examples/src/main/java/org/wikidata/wdtk/examples/JsonSerializationProcessor.java
+++ wdtk-examples/src/main/java/org/wikidata/wdtk/examples/JsonSerializationProcessor.java
@@ -29,23 +29,14 @@ import java.util.Set;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.wikidata.wdtk.datamodel.helpers.Datamodel;
-import org.wikidata.wdtk.datamodel.helpers.DatamodelConverter;
+import org.wikidata.wdtk.datamodel.helpers.DatamodelFilter;
 import org.wikidata.wdtk.datamodel.implementation.DataObjectFactoryImpl;
 import org.wikidata.wdtk.datamodel.helpers.JsonSerializer;
-import org.wikidata.wdtk.datamodel.interfaces.EntityDocumentProcessor;
-import org.wikidata.wdtk.datamodel.interfaces.ItemDocument;
-import org.wikidata.wdtk.datamodel.interfaces.ItemIdValue;
-import org.wikidata.wdtk.datamodel.interfaces.PropertyDocument;
-import org.wikidata.wdtk.datamodel.interfaces.PropertyIdValue;
-import org.wikidata.wdtk.datamodel.interfaces.Statement;
-import org.wikidata.wdtk.datamodel.interfaces.StatementGroup;
-import org.wikidata.wdtk.datamodel.interfaces.Value;
-import org.wikidata.wdtk.datamodel.interfaces.ValueSnak;
+import org.wikidata.wdtk.datamodel.interfaces.*;
 
 /**
  * This example illustrates how to create a JSON serialization of some of the
- * data found in a dump. It uses a {@link DatamodelConverter} with filter
- * settings to eliminate some of the data.
+ * data found in a dump. It uses a {@link DatamodelFilter}to eliminate some of the data.
  * <p>
  * As an example, the program only serializes data for people who were born in
  * Dresden, Germany. This can be changed by modifying the code in
@@ -64,7 +55,7 @@ public class JsonSerializationProcessor implements EntityDocumentProcessor {
 	 * Object used to make simplified copies of Wikidata documents for
 	 * re-serialization in JSON.
 	 */
-	final DatamodelConverter datamodelConverter;
+	final DatamodelFilter datamodelFilter;
 
 	/**
 	 * Runs the example program.
@@ -78,8 +69,7 @@ public class JsonSerializationProcessor implements EntityDocumentProcessor {
 		JsonSerializationProcessor.printDocumentation();
 
 		JsonSerializationProcessor jsonSerializationProcessor = new JsonSerializationProcessor();
-		ExampleHelpers
-				.processEntitiesFromWikidataDump(jsonSerializationProcessor);
+		ExampleHelpers.processEntitiesFromWikidataDump(jsonSerializationProcessor);
 		jsonSerializationProcessor.close();
 	}
 
@@ -91,29 +81,22 @@ public class JsonSerializationProcessor implements EntityDocumentProcessor {
 	 *             if there is a problem opening the output file
 	 */
 	public JsonSerializationProcessor() throws IOException {
-		// The converter is used to copy selected parts of the data. We use this
-		// to remove some parts from the documents we serialize. The converter
-		// uses a JacksonObjectFactory to be sure that we create objects for
-		// which we know how to write JSON. A similar converter without the
-		// filtering enabled would also be used to create JSON-serializable
-		// objects from any other implementation of the Wikidata Toolkit
-		// datamodel.
-		this.datamodelConverter = new DatamodelConverter(
-				new DataObjectFactoryImpl());
-		// Do not copy references at all:
-		this.datamodelConverter.setOptionDeepCopyReferences(false);
+		//Configuration of the filter
+		DocumentDataFilter documentDataFilter = new DocumentDataFilter();
 		// Only copy English labels, descriptions, and aliases:
-		this.datamodelConverter.setOptionLanguageFilter(Collections
-				.singleton("en"));
+		documentDataFilter.setLanguageFilter(Collections.singleton("en"));
 		// Only copy statements of some properties:
 		Set<PropertyIdValue> propertyFilter = new HashSet<>();
 		propertyFilter.add(Datamodel.makeWikidataPropertyIdValue("P18")); // image
 		propertyFilter.add(Datamodel.makeWikidataPropertyIdValue("P106")); // occupation
 		propertyFilter.add(Datamodel.makeWikidataPropertyIdValue("P569")); // birthdate
-		this.datamodelConverter.setOptionPropertyFilter(propertyFilter);
+		documentDataFilter.setPropertyFilter(propertyFilter);
 		// Do not copy any sitelinks:
-		this.datamodelConverter.setOptionSiteLinkFilter(Collections
-				.<String> emptySet());
+		documentDataFilter.setSiteLinkFilter(Collections.emptySet());
+
+		// The filter is used to remove some parts from the documents we
+		// serialize.
+		this.datamodelFilter = new DatamodelFilter(new DataObjectFactoryImpl(), documentDataFilter);
 
 		// The (compressed) file we write to.
 		OutputStream outputStream = new GzipCompressorOutputStream(
@@ -128,8 +111,7 @@ public class JsonSerializationProcessor implements EntityDocumentProcessor {
 	@Override
 	public void processItemDocument(ItemDocument itemDocument) {
 		if (includeDocument(itemDocument)) {
-			this.jsonSerializer.processItemDocument(this.datamodelConverter
-					.copy(itemDocument));
+			this.jsonSerializer.processItemDocument(this.datamodelFilter.filter(itemDocument));
 		}
 	}
 
