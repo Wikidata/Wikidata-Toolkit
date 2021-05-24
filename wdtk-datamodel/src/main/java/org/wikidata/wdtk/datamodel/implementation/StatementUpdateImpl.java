a3fc5329973df59efda39e56847800964dfc9d1a
Robert Važan <robert.vazan@tutanota.com>
2021-05-24 12:48:39 +0000

    Validations and tests for StatementUpdateImpl

--- wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/implementation/StatementUpdateImpl.java
+++ wdtk-datamodel/src/main/java/org/wikidata/wdtk/datamodel/implementation/StatementUpdateImpl.java
@@ -27,10 +27,14 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
 
+import org.apache.commons.lang3.Validate;
 import org.wikidata.wdtk.datamodel.helpers.Equality;
 import org.wikidata.wdtk.datamodel.helpers.Hash;
+import org.wikidata.wdtk.datamodel.interfaces.EntityIdValue;
 import org.wikidata.wdtk.datamodel.interfaces.Statement;
 import org.wikidata.wdtk.datamodel.interfaces.StatementUpdate;
 
@@ -62,13 +66,39 @@ public class StatementUpdateImpl implements StatementUpdate {
 	 * @param removed
 	 *            IDs of removed statements
 	 * @throws NullPointerException
-	 *             if any parameter is {@code null}
+	 *             if any parameter or any item is {@code null}
 	 * @throws IllegalArgumentException
 	 *             if any parameters or their combination is invalid
 	 */
 	public StatementUpdateImpl(Collection<Statement> added,
 			Collection<Statement> replaced,
 			Collection<String> removed) {
+		Objects.requireNonNull(added, "Added statement collection cannot be null.");
+		Objects.requireNonNull(replaced, "Replaced statement collection cannot be null.");
+		Objects.requireNonNull(removed, "Removed statement collection cannot be null.");
+		for (Statement statement : added) {
+			Objects.requireNonNull(statement, "Added statement cannot be null.");
+			Validate.isTrue(statement.getStatementId().isEmpty(), "Added statement cannot have an ID.");
+		}
+		for (Statement statement : replaced) {
+			Objects.requireNonNull(statement, "Replaced statement cannot be null.");
+			Validate.notBlank(statement.getStatementId(), "Replaced statement must have an ID.");
+		}
+		for (String id : removed) {
+			Validate.notBlank(id, "Removed statement ID cannot be null or blank.");
+		}
+		long distinctIds = Stream
+				.concat(replaced.stream().map(s -> s.getStatementId()), removed.stream())
+				.distinct()
+				.count();
+		Validate.isTrue(replaced.size() + removed.size() == distinctIds, "Statement IDs must be unique.");
+		Validate.isTrue(
+				Stream.concat(added.stream(), replaced.stream()).map(s -> s.getSubject()).distinct().count() <= 1,
+				"All statements must have the same subject.");
+		EntityIdValue subject = Stream.concat(added.stream(), replaced.stream())
+				.map(s -> s.getSubject())
+				.findFirst().orElse(null);
+		Validate.isTrue(subject == null || !subject.isPlaceholder(), "Cannot update entity with placeholder ID.");
 		this.added = Collections.unmodifiableList(new ArrayList<>(added));
 		this.replaced = Collections.unmodifiableMap(replaced.stream().collect(toMap(s -> s.getStatementId(), s -> s)));
 		this.removed = Collections.unmodifiableSet(new HashSet<>(removed));
